4a78744866d5627eb786afb45ffa30aa25170bc9
Devops <devopsInabox@newtglobal.com>
2017-08-27 11:50:15 +0530

    Update HomeController.java

--- src/main/java/com/newt/ecom/HomeController.java
+++ src/main/java/com/newt/ecom/HomeController.java
@@ -5,7 +5,6 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.converter.StringHttpMessageConverter;
